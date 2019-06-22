c27bde3ad5a6e55233fe7bf8d0370366eebde179
Fann1k <faniskhantimirov@gmail.com>
2019-06-22 19:51:34 +0300

    done

--- hamburger_native_js/app/js/script.js
+++ hamburger_native_js/app/js/script.js
@@ -21,59 +21,10 @@ overlay.addEventListener('click', () => {
 
 let toggleMenu = () => {
     if (menu.classList.contains('burger-menu_active')) {
-        disable_scroll();
         menu.classList.remove('burger-menu_active');
+        body.style.overflow = '';
     } else {
-        enable_scroll();
         menu.classList.add('burger-menu_active');
+        body.style.overflow = 'hidden';
     }
 };
-
-
-function preventDefault(e) {
-    e = e || window.event;
-    if (e.preventDefault)
-        e.preventDefault();
-    e.returnValue = false;
-}
-
-function keydown(e) {
-    for (var i = keys.length; i--;) {
-        if (e.keyCode === keys[i]) {
-            preventDefault(e);
-            return;
-        }
-    }
-}
-
-function wheel(e) {
-    preventDefault(e);
-}
-
-function disable_scroll() {
-    if (window.addEventListener) {
-        window.addEventListener('DOMMouseScroll', wheel, false);
-    }
-    window.onmousewheel = document.onmousewheel = wheel;
-    document.onkeydown = keydown;
-    disable_scroll_mobile();
-}
-
-function enable_scroll() {
-    if (window.removeEventListener) {
-        window.removeEventListener('DOMMouseScroll', wheel, false);
-    }
-    window.onmousewheel = document.onmousewheel = document.onkeydown = null;
-    enable_scroll_mobile();
-}
-
-// My improvement
-
-// MOBILE
-function disable_scroll_mobile() {
-    document.addEventListener('touchmove', preventDefault, false);
-}
-
-function enable_scroll_mobile() {
-    document.removeEventListener('touchmove', preventDefault, false);
-}
