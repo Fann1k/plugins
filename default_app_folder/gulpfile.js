daaa1a4e31758decc48530571fed68a0952ab1ad
Хантимиров Фанис Минегалиевич <Khantimirov_FM@kvz.ru>
2019-07-09 12:27:37 +0300

    change default_app_folder

--- default_app_folder/gulpfile.js
+++ default_app_folder/gulpfile.js
@@ -2,23 +2,30 @@ const gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     cleanCSS = require('gulp-clean-css'),
     config = {
-        root: './src/',
+        root: './app/',
         css: {
-            watch: 'precss/**/*.less',
+            watchLess: 'precss/**/*.less',
+            watchCss: 'precss/**/*.css',
             src: 'precss/styles.less',
             dest: 'css'
         },
-        html: '*.html'
+        html: '*.html',
+        js: {
+            src: 'prejs/**/*.js',
+            dest: 'js'
+        }
     },
     sourcemaps = require('gulp-sourcemaps'),
     browserSync = require('browser-sync').create(),
     rename = require("gulp-rename"),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
+    terser = require('gulp-terser');
 
 
-gulp.task('build', function (done) {
+gulp.task('buildCSS', function (done) {
     gulp.src(config.root + config.css.src)
-
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(autoprefixer({
@@ -40,8 +47,30 @@ gulp.task('build', function (done) {
     done();
 });
 
+gulp.task('buildJS', function (done) {
+    gulp.src(config.root + config.js.src)
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('main.js'))
+        .pipe(terser({
+            ie8: true
+        }))
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.root + config.js.dest))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+    done();
+});
+
 gulp.task('watch:styles', function () {
-    gulp.watch(config.root + config.css.watch, gulp.series('build')); //берет ./src/precss/**/*.css делает с ним 'build' + можно еще что-то после build добавить
+    gulp.watch(config.root + config.css.watchLess, gulp.series('buildCSS')); //берет ./src/precss/**/*.css делает с ним 'buildCSS' + можно еще что-то после buildCSS добавить
+    gulp.watch(config.root + config.css.watchCss, gulp.series('buildCSS'));
 });
 gulp.task('watch:html', function () {
     gulp.watch(config.root + config.html).on('change', browserSync.reload);
@@ -56,4 +85,4 @@ gulp.task('browser-sync', function () {
     });
 });
 
-gulp.task('watch', gulp.series('build', gulp.parallel('watch:styles', 'watch:html', 'browser-sync')));
+gulp.task('watch', gulp.series('buildCSS', gulp.parallel('watch:styles', 'watch:html', 'browser-sync')));
